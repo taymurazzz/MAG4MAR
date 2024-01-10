b3dff3cd0e2c85661add3a4c18ad8591ac36cace
taymurazzz <anothertahmuraz@gmail.com>
2024-01-10 21:37:06 +0300

    new

--- main.js
+++ main.js
@@ -1,90 +1,134 @@
-class Product{
-    Name;
-    Category;
-    Height;
-    FootSize;
-    constructor(name, category, height, footSize){
-        Name=name;
-        Category=category;
-        Height=height;
-        FootSize=footSize;
+class product{
+    name;
+    category;
+    height;
+    footsize;
+    constructor(name, category, height, footsize){
+        this.name=name;
+        this.category=category;
+        this.height=height;
+        this.footsize=footsize;
     }
 }
 
-let products=new Array(10);
+let ctlg=new Array(10);
+ctlg[0]=new product('1', 'кросcовки', '0', '35' );
+ctlg[1]=new product('2', 'кросcовки', '0', '35' );
+ctlg[2]=new product('3', 'кросcовки', '0', '36' );
+ctlg[3]=new product('4', 'кросcовки', '0', '36');
+ctlg[4]=new product('5', 'кросcовки', '0', '37' );
+ctlg[5]=new product('6', 'кросcовки', '0', '37' );
+ctlg[6]=new product('7', 'верх', '185', '0');
+ctlg[7]=new product('8', 'верх', '185', '0');
+ctlg[8]=new product('9', 'верх', '185', '0');
+ctlg[9]=new product('10', 'верх', '185', '0');
 
-products[0]={
-    Name: '1', 
-    Category: 'shoes', 
-    Height: '0', 
-    FootSize: '36' 
-}
-products[1]={
-    Name: '2', 
-    Category: 'shoes', 
-    Height: '0', 
-    FootSize: '36' 
-}
-products[2]={
-    Name: '3', 
-    Category: 'shoes', 
-    Height: '0', 
-    FootSize: '36' 
-}
-products[3]={
-    Name: '4', 
-    Category: 'shoes', 
-    Height: '0', 
-    FootSize: '36' 
-}
-products[4]={
-    Name: '5', 
-    Category: 'shoes', 
-    Height: '0', 
-    FootSize: '36' 
-}
-products[5]={
-    Name: '6', 
-    Category: 'shoes', 
-    Height: '0', 
-    FootSize: '36' 
-}
-products[6]={
-    Name: '7', 
-    Category: 'shoes', 
-    Height: '0', 
-    FootSize: '36' 
-}
-products[7]={
-    Name: '8', 
-    Category: 'shoes', 
-    Height: '0', 
-    FootSize: '36' 
-}
-products[8]={
-    Name: '9', 
-    Category: 'shoes', 
-    Height: '0', 
-    FootSize: '36' 
-}
-products[9]={
-    Name: '10', 
-    Category: 'shoes', 
-    Height: '0', 
-    FootSize: '36' 
-}
+let userList;
 
-localStorage.setItem('pr', JSON.stringify(products[2]));
-
-document.querySelector('.button_save').addEventListener('click', ()=>{
-    let data = document.querySelector('.height').value;
-    localStorage.setItem('height', data);
-    let userParams = {
-        height: document.querySelector('.height').value,
-        footSize: document.querySelector('.foot_size').value
+let user = new product('0', '0', '0', '0'); // работает и без лет
+let userJSON;
+let storedUserJSON;
+let storedUser;
+let path = "res/";
+let GridList = document.getElementById('grid-list');
+storedUserJSON = localStorage.getItem('user');
+if(storedUserJSON!=null){
+    storedUser = JSON.parse(storedUserJSON);
+    userList = [];
+    for(let i=0; i<10; i++){
+        if (ctlg[i].category == "верх" && ctlg[i].height == storedUser.height){
+            userList.push(ctlg[i]);
+        }
+        if (ctlg[i].category == "кросcовки" && ctlg[i].footsize == storedUser.footsize){
+            userList.push(ctlg[i]);
+        }
     }
-    localStorage.setItem('userParams', JSON.stringify(userParams));
+    for(let i=0; i<userList.length; i++){
+        let li = document.createElement("li");
+        li.className = "grid-item";
+        li.style.display = "flex";
+        li.style.flexDirection = "column";
+        li.style.border = "3px solid black";
+        const img = document.createElement("img");
+        img.src = path+(userList[i].name)+".jpg";
+        const p = document.createElement("p");
+        p.textContent =userList[i].category+" " + userList[i].footsize +" "+ userList[i].height ;
+        li.appendChild(img);
+        li.appendChild(p);
+        GridList.appendChild(li);
+    }
+}
+if(storedUserJSON==null){
+    ViewAll();
+}
+
+document.getElementById('userProfileForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    user.height = document.getElementById('height').value,
+    user.footsize = document.getElementById('footSize').value;
+    userJSON = JSON.stringify(user);
+    localStorage.setItem('user', userJSON);
+    storedUserJSON = localStorage.getItem('user');
+    storedUser = JSON.parse(storedUserJSON);
+    userList = [];
+    for(let i=0; i<10; i++){
+        if (ctlg[i].category == "верх" && ctlg[i].height == storedUser.height){
+            userList.push(ctlg[i]);
+        }
+        if (ctlg[i].category == "кросcовки" && ctlg[i].footsize == storedUser.footsize){
+            userList.push(ctlg[i]);
+        }
+    }
+    while (GridList.firstChild) {
+        GridList.removeChild(GridList.firstChild);
+    }
+    // }
+    for(let i=0; i<userList.length; i++){
+        let li = document.createElement("li");
+        li.className = "grid-item";
+        li.style.display = "flex";
+        li.style.flexDirection = "column";
+        li.style.border = "3px solid black";
+        const img = document.createElement("img");
+        img.src = path+(userList[i].name)+".jpg";
+        const p = document.createElement("p");
+        p.textContent =userList[i].category+" " + userList[i].footsize +" "+ userList[i].height ;
+        li.appendChild(img);
+        li.appendChild(p);
+        GridList.appendChild(li);
+    }
 })
-document.querySelector('.button_reset').addEventListener('click', ()=>{
-    localStorage.clear();
-})
+
+function clearUser(){
+    localStorage.removeItem('user');
+    ViewAll();
+}
+function ViewAll(){
+    while (GridList.firstChild) {
+        GridList.removeChild(GridList.firstChild);
+    }
+    userList = [];
+    for(let i=0; i<10; i++){
+        userList.push(ctlg[i]);
+    }
+    for(let i=0; i<userList.length; i++){
+        let li = document.createElement("li");
+        li.className = "grid-item";
+        li.style.display = "flex";
+        li.style.flexDirection = "column";
+        li.style.border = "3px solid black";
+        const img = document.createElement("img");
+        img.src = path+(userList[i].name)+".jpg";
+        const p = document.createElement("p");
+        
+        p.textContent =userList[i].category+" " + userList[i].footsize +" "+ userList[i].height ;
+        li.appendChild(img);
+        li.appendChild(p);
+        GridList.appendChild(li);
+    }
+}
+
+const removeMyDataDiv = document.querySelector('.removeMyData');
+const showAllRightNowDiv = document.querySelector('.showAllRightNow');
+removeMyDataDiv.addEventListener('click', clearUser);
+showAllRightNowDiv.addEventListener('click', ViewAll);
